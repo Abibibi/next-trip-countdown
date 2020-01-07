0b9a0df564d0352b814481b2c61e32fbd9930f33
Abibibi <abeba.ngwe@gmail.com>
2020-01-07 17:49:16 +0100

    refactoring form fields, by created a Field component and a _field.sass file

---
+++ front-end/src/components/Field.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input
+      class="form-content-input"
+      type="text"
+      name="firstname"
+      id="firstname"
+      title="Saisissez votre prénom"
+      required
+    >
+    <label for="firstname">
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    labelText: String
+  },
+  data () {
+    return {
+      label: this.labelText
+    }
+  }
+}
+</script>
