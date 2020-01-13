1ff58644ba69f0ffbcb579b240a160c772562b58
Abibibi <abeba.ngwe@gmail.com>
2020-01-13 02:10:38 +0100

    created Welcome view (to display homepage for users who submitted the form) and linked routes to logged boolean property from state

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import Welcome from '@/views/Welcome.vue'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -8,7 +10,18 @@ const routes = [
   {
     path: '/',
     name: 'home',
+    meta: {
+      requiresVisitor: true
+    },
     component: Home
+  },
+  {
+    path: '/',
+    name: 'welcome',
+    meta: {
+      requiresAuth: true
+    },
+    component: Welcome
   }
 ]
 
@@ -16,6 +29,28 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.state.logged) {
+      next({
+        name: 'home'
+      })
+    } else {
+      next()
+    }
+  } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+    if (store.state.logged) {
+      next({
+        name: 'welcome'
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
--- front-end/src/store/state.js
+++ front-end/src/store/state.js
@@ -2,5 +2,5 @@ export default {
   firstname: '',
   city: '',
   date: '',
-  logged: false
+  logged: true
 }
