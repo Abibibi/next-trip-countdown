0fdbef650ed8f3826da2a77507da59bc35ef1ce1
Abibibi <abeba.ngwe@gmail.com>
2020-01-13 15:56:52 +0100

    created Picture component for refactoring purposes (going to use Picture component both in Home and Welcome views, since Picture holds the picture that is displayed in both views, in the background)

---
+++ front-end/src/components/Picture.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="picture">
+    <img class="picture-content" :alt="alt" :src="photo">
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    picture: String,
+    pictureAlt: String
+  },
+  data () {
+    return {
+      photo: this.picture,
+      alt: this.pictureAlt
+    }
+  }
+}
+</script>
